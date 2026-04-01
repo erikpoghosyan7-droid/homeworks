a86c7b0731010072f9b44720742e2fee6e41624c
erikpoghosyan7-droid <erikpoghosyan7@gmail.com>
2026-04-01 13:35:37 +0400

    new 1.04

--- homework0803.js
+++ homework0803.js
@@ -29,9 +29,99 @@ function map1(array,callback){
 
     
    let example = map1([2,6,10],function(x) {
-     return x++ ;
-   });
-    console.log(example);
+      return x+=2 ;
+      });
+
+      console.log(example);
+
+
+function filter1(array, callback){
+     
+       let newarr = [];
+       for(let i = 0; i < array.length; i++){
+        
+       if(callback(array[i], i, array)){
+           newarr.push(array[i]);
+       }
+       
+    }
+
+    return newarr;
+}
+
+     
+      let example1 = filter1([2,9,12,4,7], function(x){
+
+            return (x % 2 === 0);
+      });
+
+      console.log(example1);
+
+
+
+function some1(array, callback){
+         
+       let bol = false;
+       for(let i = 0; i < array.length; i++){
+        
+          if(callback(array[i], i, array)){
+             
+             bol = true;
+             
+              
+         }
+            
+    }
+
+    return bol;
+}
+
+     
+      let example2 = some1([7,9,11,1,-3], function(x){
+
+            return (x % 2 === 0);
+      });
+
+      console.log(example2);
 
     
+   function myEvery(array,callback){
 
+       let res = true; 
+       for(let i = 0; i < array.length; i++){
+
+           if(!callback(array[i], i, array)){
+               
+              res = false;
+
+           }
+       }
+           return res;
+
+   }       
+
+   let ever = myEvery([1,5,7,9], function(x){
+
+         return x < 9;
+   });
+
+   console.log(ever);
+
+
+   function myIndexof(array, searchElement){
+
+      let strres = -1;
+       for(i = 0; i < array.length; i++){
+          
+           if(searchElement===(array[i])){
+
+               strres = i;
+           }
+       }
+          
+          return strres;
+   }
+
+
+   let exm = myIndexof([1,5,8,3,2,5], 1);
+   console.log(exm);
